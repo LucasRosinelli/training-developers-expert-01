0c5b051b7966b2500273c5641afda4f88a1a80b1
Lucas Rosinelli da Rocha <lucas.rosinelli@gmail.com>
2020-11-18 02:14:26 +0000

    Add backend
    - Added backend in .NET 5

---
+++ backend/Training.DevelopersExpert/src/Training.DevelopersExpert.Api/Controllers/AlbumsController.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Training.DevelopersExpert.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AlbumsController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAlbums()
+        {
+            return await Task.FromResult(Ok(new
+            {
+                Message = "My first API",
+            }));
+        }
+    }
+}
